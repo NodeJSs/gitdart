8d67a891cf65cce7d215d58aaf136ea5d898e108
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-23 14:10:37 +0000

    finished

--- lib/gitdartCli.dart
+++ lib/gitdartCli.dart
@@ -5,9 +5,20 @@ import 'package:gitdart/models/User.dart';
 
 void gitdartCli(String users){
   List<String> _users = users.split(",");
-  for(var user in _users){
-    fetchUserData(user).then((User user){
-      stdout.writeln(user.name);
+  stdout.writeln("Fetching user(s)...");
+  var userCount = 1;
+  for(var username in _users){
+    fetchUserData(username).then((User user){
+      stdout.writeln("User #${(userCount).toString()}");
+      stdout.writeln("Username: ${username}"); 
+      stdout.writeln("Name: ${user.name}"); 
+      stdout.writeln("Location: ${user.location}"); 
+      stdout.writeln("Bio: ${user.bio}"); 
+      stdout.writeln("Public Repos: ${user.publicRepos}"); 
+      stdout.writeln("Followers: ${user.followers}"); 
+      stdout.writeln("Following: ${user.following}"); 
+      stdout.writeln("  ");
+      userCount = userCount+1;
     });
     
   }
